ac87a90f9ea6e66e41c78373652b650584df3295
elvinasd <42936456+elvinasdidika@users.noreply.github.com>
2023-06-02 16:32:14 +0300

    Create test_shipment.py

---
+++ test_shipment.py
@@ -0,0 +1,15 @@
+import unittest
+from shipment import Shipment
+
+class TestShipment(unittest.TestCase):
+    def test_create_shipment(self):
+        # Test the creation of a shipment
+        pass
+
+    def test_get_price(self):
+        # Test the retrieval of a shipment price
+        pass
+
+# Run the tests
+if __name__ == "__main__":
+    unittest.main()
